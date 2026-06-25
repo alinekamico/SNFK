61f59068a836222f9e8f2096902772bc7d75bd91
Aline <aline@kamico.com.br>
2026-06-25 13:35:16 -0300

    fix: import relationship em usuario.py

--- backend/app/models/usuario.py
+++ backend/app/models/usuario.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, Boolean, DateTime, ForeignKey, Enum
+from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.mysql import CHAR
 import uuid
 from datetime import datetime
